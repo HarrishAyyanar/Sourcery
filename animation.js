848fff260116e5fe60babc2f44f8b3fc5ceb12e3
Sourceryforeveryone <97740837+Sourceryforeveryone@users.noreply.github.com>
2022-01-14 19:55:20 +0530

    Update animation.js

--- animation.js
+++ animation.js
@@ -10,8 +10,8 @@ gsap.utils.toArray('.section').forEach(section => {
 });
 
 function restrictWidth(){
-  if (window.innerWidth < 1000) {
- alert("The website is not compatible for devices other than laptop or for browser sizes below 1000px. Please view this website on laptop or at a browser size of more than 1000px")
+  if (window.innerWidth < 1100) {
+ alert("The website is not compatible for devices other than laptop or for browser sizes below 1100px. Please view this website on laptop or at a browser size of more than 1100px")
 } else {
 } 
 }
